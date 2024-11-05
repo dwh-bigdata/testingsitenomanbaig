f79fb4fe41245cb3f6271e5cf2d6452d6d174781
dwh-bigdata <bigdata.analytics.bafl@gmail.com>
2024-11-05 14:51:15 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.querySelector('button');
+    button.addEventListener('click', function () {
+        alert('Thanks for clicking!');
+    });
+});

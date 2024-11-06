4985779aa64ba2d86e474d46c413b43ccdd9eb54
dwh-bigdata <bigdata.analytics.bafl@gmail.com>
2024-11-06 13:52:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 // script.js
 
-// Function to send button click events to Google Analytics
+// Function to send button-specific click events to Google Analytics
 function trackButtonClick(buttonName) {
-    // Send event data to Google Analytics
-    gtag('event', 'click', {
+    // Send event data with a custom event name to Google Analytics
+    gtag('event', buttonName, {
         'event_category': 'Buttons',
         'event_label': buttonName,
         'value': 1
     });
 }
+
+// Adding event listeners for each button with unique event names
+document.getElementById('button1').addEventListener('click', () => trackButtonClick('button1_clicked'));
+document.getElementById('button2').addEventListener('click', () => trackButtonClick('button2_clicked'));
+document.getElementById('button3').addEventListener('click', () => trackButtonClick('button3_clicked'));
